a431773bdec9d9b0237f438587f1111029fc24e3
inmyownwords7 <andrew.j.lansangan@gmail.com>
2023-11-25 23:53:33 -0500

    missing files

--- app.js
+++ app.js
@@ -1,21 +1,76 @@
+const __dirname = require('path').dirname;
+
+const dotenv = require('dotenv').config({path: path.join(__dirname, '')});
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const session = require('express-session')
+const passport = require('passport');
+const winston = require('winston');
+
+// const exphbs = require('express-handlebars')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/users');
+const {configDotenv} = require("dotenv");
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+
+// let username = "admin"
+// let password = ""
+// let cluster-name = ""
+// let dbname = ""
+
+let username = process.env.USERNAME;
+let password = process.env.PASSWDORD;
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+// const sessionSecret = process.env.SESSION_SECRET || 'secret';
+
+// mongoose.connect(`mongodb+srv//${username}:${password}@usersivsjmrs.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
 const app = express();
+//const userStrategy = new LocalStrategy();
+
+// error handler
+passport.use("app", LocalStrategy);
+
+app.use(passport.initialize(undefined));
+app.use(passport.session(undefined));
+
+passport.serializeUser((user, done) => {
+done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+})
+
+app.use(logger('combined', {stream: accessLogStream}));
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: false
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+// app.engine('handlebars')
+// app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,6 +83,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
+// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -37,5 +93,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
+//const session = require('express-session');
+const passport = require('passport');
+
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Login', user: req.user });
+});
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+// Middleware to check if the user is authenticated
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+router.get('/dashboard', isAuthenticated, (req, res) => {
+  res.render('dashboard', { user: req.user });
 });
 
 module.exports = router;
